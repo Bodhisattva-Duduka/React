c16bb88686260288c42321182c812a5d313c664c
Bodhisattva Duduka <bodhisattva.duduka@gmail.com>
2026-04-06 10:17:07 +0530

    little bit of practice

---
+++ Basics of React/my-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function Title() {
+  return (
+    <h2>I'm Title</h2>
+  )
+}
+
+function Button() {
+  return (
+    <button type="button">click me</button>
+  )
+}
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <h1>This is React</h1>
+      <Title />
+    </>
+
+  )
+}
+
+export default App
